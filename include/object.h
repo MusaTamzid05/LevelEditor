c9d4ad001f2e43b6b0856b1b308b3779688cee02
MusaTamzid05 <tamzid101@gmail.com>
2024-05-28 16:22:11 +0600

    Define Object

---
+++ include/object.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+struct Object {
+    Object();
+    virtual ~Object();
+
+    virtual void init() = 0;
+    virtual void render() = 0;
+    virtual void update() = 0;
+};
+
+#endif
